3ac18c130eba52679939a7c7c2fbba0bccdd1131
Rahul Laxminarayan Mutyalkar <rmutyalkar@GHS.LOCAL>
2020-03-11 15:02:07 +0530

    add custom validation and add other input fields

--- src/app/registeration/registeration.component.ts
+++ src/app/registeration/registeration.component.ts
@@ -13,18 +13,50 @@ export class RegisterationComponent implements OnInit {
   constructor(public fb: FormBuilder) { }
 
   ngOnInit() {
-    this.initForm()
+    this.initForm();
+
   }
 
   initForm() {
     this.registerationForm = this.fb.group({
-      email: ['', [Validators.required]],
+      firstName: ['', [Validators.required, Validators.pattern('^[\w ]+$')]],
+      lastName: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}')]],
       password: ['', [Validators.required]],
-      gender: ['', [Validators.required]]
-    })
+      confirmPassword: ['', [Validators.required]],
+      gender: ['', [Validators.required]],
+      mobile: [null, [Validators.required]]
+    },
+      //{ validator: this.passwordMatchValidator }
+    );
+
   }
 
-  checkMe() {
-    console.log('checkMe() : ', this.name);
+  passwordMatchValidator(form) {
+    console.log('form : ', form);
+
+    if (form.controls['password'].value !== form.controls['confirmPassword'].value) {
+      form.controls['confirmPassword'].setErrors({ 'mismatch': true });
+      return false
+    }
+    return true;
+  }
+
+  // checkMe() {
+  //   console.log('checkMe() : ', this.name);
+  // }
+
+  submit() {
+    console.log('registeration form : ', this.registerationForm.controls)
+    // this.registerationForm.setValue({ email: 'rahulmutyalkar@gmail.com' }, { emitEvent: true });
+    // this.registerationForm.patchValue({ email: 'rahulmutyalkar@gmail.com', mobile: '9673412006', gender: 'male' })
+    // this.registerationForm = this.registerationForm;
+    // console.log('this.registerationForm : ', this.registerationForm.controls)
+    // this.registerationForm.updateValueAndValidity();
+  }
+
+  setPristineToAll() {
+
+
   }
 }
